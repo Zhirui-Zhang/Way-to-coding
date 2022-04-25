3c6bde9998f53dd9361c6e4e4ed5bfa8e90fcf8f
Zhirui-Zhang <h853201808@qq.com>
2022-04-25 18:15:24 +0800

    Update Solution 0146

--- LeetCode 热题 Hot 100/0146. LRU 缓存(medium).cpp
+++ LeetCode 热题 Hot 100/0146. LRU 缓存(medium).cpp
@@ -11,14 +11,14 @@ struct LinkedNode
     LinkedNode* prev;
     LinkedNode* next;
     // 定义两个常用的构造函数，便于后续初始化，注意写法，后面没有分号，而是函数体大括号！！！
-    LinkedNode(int k, int v) : key(k), value(v), prev(nullptr), next(nullptr){}
-    LinkedNode() : key(0), value(0), prev(nullptr), next(nullptr){}        
+    LinkedNode(int k, int v) : key(k), value(v), prev(nullptr), next(nullptr) {}
+    LinkedNode() : key(0), value(0), prev(nullptr), next(nullptr) {}        
 };      // 注意定义后的分号！！！
 
 class LRUCache {
 private:
     unordered_map<int, LinkedNode*> un_map;
-    LinkedNode* head;     // 声明链表中的首尾节点，注意都为哑节点，仅仅为方便元素插入和删除使用，不用在判断是否存在该节点
+    LinkedNode* head;     // 声明链表中的首尾节点，注意都为哑节点，仅仅为方便元素插入和删除使用，不用再判断是否存在该节点
     LinkedNode* tail;
     int cur;
     int total;
@@ -52,9 +52,10 @@ public:
             addToHead(node);        // 不管内存是否已满，都需要把新节点加入到头节点之后
             if(cur > total)         // 若内存已满，需要删除尾部前一个元素，插入新元素
             {
-                LinkedNode* temp = removeTail();
+                LinkedNode* temp = removeTail();    // 接收返回了的tail的前一个节点
                 un_map.erase(temp->key);    // 在这里链表结构中存储key值的作用就体现出来了，便于哈希表中删除该项，注意删除写法，只写key值即可
-                delete temp;        // 这里很关键，在使用完temp节点后需要删除掉，防止内存泄漏，即temp变成野指针
+                delete temp;        
+                // 这里很关键，在使用完temp节点后需要删除掉，防止内存泄漏，即temp变成野指针，此时才相当于removeTail函数中删除掉了node节点
             }
         }
         else        // 若存在，直接修改并更新即可
@@ -77,6 +78,7 @@ public:
     {
         node->prev->next = node->next;
         node->next->prev = node->prev;
+        // 注意这里不能直接删除node节点，因为在updateList函数，需要将node移至头部，removeTail函数需要返回，都有用途故不能delete
     }
 
     void updateList(LinkedNode* node)
@@ -88,7 +90,7 @@ public:
     LinkedNode* removeTail()
     {
         LinkedNode* node = tail->prev;
-        removeNode(node);
+        removeNode(node);       // 这里并不是真删除node节点，只是把node前后的节点指针修改一下，最后返回node，用于哈希表的更新
         return node;
     }
 };
