2758657509d458fb842e4bdb3b257f225d1d76b7
Zhirui-Zhang <h853201808@qq.com>
2022-04-28 16:18:19 +0800

    Update Solution 0287

--- LeetCode 热题 Hot 100/0283. 移动零(easy).cpp
+++ LeetCode 热题 Hot 100/0283. 移动零(easy).cpp
@@ -29,15 +29,11 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        int i = 0, j = 0;
-        while(j < n)
-        {
-            if(nums[j] != 0)
-            {
+        for (int i = 0, j = 0; j < n; ++j) {
+            if (nums[j] != 0) {
                 swap(nums[i], nums[j]);
-                ++i;
+                i++;
             }
-            ++j;
         }
         return;
     }
