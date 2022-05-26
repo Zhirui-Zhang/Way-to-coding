17a3531e469bf7a0413e578ef3a0d4dbc45d96e7
Zhirui-Zhang <h853201808@qq.com>
2022-05-26 12:51:06 +0800

    Update Solution 0070

--- LeetCode 热题 Hot 100/0070. 爬楼梯(easy).cpp
+++ LeetCode 热题 Hot 100/0070. 爬楼梯(easy).cpp
@@ -15,3 +15,18 @@ public:
         return c;
     }
 };
+
+拓展：如果改为一步可迈m阶台阶（m < n），一共有多少种方法，就变成了一道完全背包的问题，且由于是排列问题，应该外层背包容量，内层物品
+class Solution {
+public:
+    int climbStairs(int n, int m) {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;      // 初始化dp[0] = 1
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {       // 而这道题只是m = 2的特殊情况而已
+                if (i >= j) dp[i] += dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+};
