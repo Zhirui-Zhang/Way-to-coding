6aee57f0e603eb13c2d7f873e9a314f4be03a8c3
Zhirui-Zhang <h853201808@qq.com>
2022-05-27 15:22:36 +0800

    Update Solution 0309

--- LeetCode 热题 Hot 100/0309. 最佳买卖股票时机含冷冻期(medium).cpp
+++ LeetCode 热题 Hot 100/0309. 最佳买卖股票时机含冷冻期(medium).cpp
@@ -18,6 +18,24 @@ public:
     }
 };
 
+代码随想录的DP数组代码示例，比较统一，免得初始化时还要设置INT_MIN：
+执行用时：12 ms, 在所有 C++ 提交中击败了22.60% 的用户
+内存消耗：11.5 MB, 在所有 C++ 提交中击败了11.63% 的用户
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector(3, 0));    
+        dp[0][1] = -prices[0];          // 注意初始化时设置第一天买入值dp[0][1] = -prices[0]                         
+        for(int i = 1; i < n; ++i) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2]);             // dp[i][0]表示当天不买入/卖出任何股票，但有可能是前一天冷冻期转移过来的
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]); // dp[i][1]表示当天持有股票，可能当天买入/前一天持有的
+            dp[i][2] = dp[i - 1][1] + prices[i];                    // dp[i][2]表示当天为冷冻期，一定是卖出了前一天的股票
+        }
+        return max(dp[n - 1][0], dp[n - 1][2]);                     // 最后返回手中无股票两种情况中的较大值     
+    }
+};
+
 改进版：将dp数组转化为3个状态量，注意循环之前存储一下临时变量a b即可
 时间O(N)  空间O(1)
 执行用时：0 ms, 在所有 C++ 提交中击败了100.00% 的用户
@@ -35,5 +53,4 @@ public:
         }
         return max(a, c);
     }
-};
 };
