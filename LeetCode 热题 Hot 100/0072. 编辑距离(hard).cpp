7fe2b18faaaa7f167bfbb9e9d058e9bcc97ba607
Zhirui-Zhang <h853201808@qq.com>
2022-05-31 21:02:33 +0800

    Update Solution 0072

--- LeetCode 热题 Hot 100/0072. 编辑距离(hard).cpp
+++ LeetCode 热题 Hot 100/0072. 编辑距离(hard).cpp
@@ -21,3 +21,24 @@ public:
         return dp[m][n];
     }
 };
+
+学习代码随想录后，自己写的模板更新，与583题唯一的不同之处在于当对应字符不同时，还需要考虑↖方向的值即替换之前的一个字符，所以dp值为三个方向上取最小值+1
+时间O(MN)  空间O(MN)
+执行用时：24 ms, 在所有 C++ 提交中击败了11.44% 的用户
+内存消耗：8.7 MB, 在所有 C++ 提交中击败了70.44% 的用户
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
+        for (int i = 1; i < m + 1; ++i)    dp[i][0] = i;
+        for (int j = 1; j < n + 1; ++j)    dp[0][j] = j;
+        for (int i = 1; i < m + 1; ++i) {
+            for (int j = 1; j < n + 1; ++j) {
+                if (word1[i - 1] == word2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+                else dp[i][j] = min({dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]}) + 1;
+            }
+        }
+        return dp[m][n];
+    }
+};
