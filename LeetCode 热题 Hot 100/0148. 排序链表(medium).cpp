4395635e28c7aa5bc57998e404d3bab7861d0dd8
Zhirui-Zhang <h853201808@qq.com>
2022-07-04 14:35:49 +0800

    Update Solution 0148

--- LeetCode 热题 Hot 100/0148. 排序链表(medium).cpp
+++ LeetCode 热题 Hot 100/0148. 排序链表(medium).cpp
@@ -1,4 +1,4 @@
-法1：归并排序法，但是与通常的归并不同，这次是空间复杂度为O(1)的方法，与数组不同，链表可以直接通过改变指针方向进行排序，因此可以实现O(1)
+法1：（迭代版）自底向上的归并排序法，但是与通常的归并不同，这次是空间复杂度为O(1)的方法，与数组不同，链表可以直接通过改变指针方向进行排序，因此可以实现O(1)
 时间O(NlogN)  空间O(1)
 执行用时：128 ms, 在所有 C++ 提交中击败了13.19% 的用户
 内存消耗：31.8 MB, 在所有 C++ 提交中击败了57.79% 的用
@@ -59,6 +59,47 @@ private:
             }
         }
         temp->next = l1 ? l1 : l2;
+        return dummy->next;
+    }
+};
+
+法2：（递归版）自顶向下的递归排序法，先递归至单个节点，再依次向上递归，复杂度更高，注意沿途记录快慢节点的位置
+时间O(NlogN)  空间O(logN)
+执行用时：180 ms, 在所有 C++ 提交中击败了31.15% 的用户
+内存消耗：72.3 MB, 在所有 C++ 提交中击败了15.89% 的用户
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode *slow = head, *fast = head, *pre = head;
+        while (fast && fast->next) {
+            // 记录slow的前驱，针对链表有偶数个结点的情况
+            pre = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }       
+        pre->next = nullptr;    // 注意切断slow前一个节点
+        ListNode *l1 = sortList(head);
+        ListNode *l2 = sortList(slow);
+        return mergeSort(l1, l2);
+    }
+
+private:
+    ListNode* mergeSort(ListNode* l1, ListNode* l2) {
+        ListNode *dummy = new ListNode(0);
+        ListNode *pre = dummy;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                pre->next = l1;
+                l1 = l1->next;
+                pre = pre->next;
+            } else {
+                pre->next = l2;
+                l2 = l2->next;
+                pre = pre->next;
+            }
+        }
+        pre->next = l1 ? l1 : l2;
         return dummy->next;
     }
 };
